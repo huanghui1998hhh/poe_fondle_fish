01c564b4ec4cb253c9bfa38ed1eb9cff5f3e2132
huanghui1998hhh <1224991097@qq.com>
2021-01-18 19:37:39 +0800

    test

--- lib/tab/poe_db_page.dart
+++ lib/tab/poe_db_page.dart
@@ -1,10 +1,37 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-class PoeDbPage extends StatelessWidget {
-  const PoeDbPage({Key key}) : super(key: key);
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class PoeDbPage extends StatefulWidget {
+  PoeDbPage({Key key}) : super(key: key);
+
+  @override
+  _PoeDbPageState createState() => _PoeDbPageState();
+}
+
+class _PoeDbPageState extends State<PoeDbPage>
+    with AutomaticKeepAliveClientMixin {
+  Completer<WebViewController> _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = Completer<WebViewController>();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return WebView(
+      initialUrl: 'https://poedb.tw/cn',
+      javascriptMode: JavascriptMode.unrestricted,
+      onWebViewCreated: (WebViewController webViewController) {
+        _controller.complete(webViewController);
+        setState(() {});
+      },
+    );
   }
+
+  @override
+  bool get wantKeepAlive => true;
 }
--- lib/tab/trade_page.dart
+++ lib/tab/trade_page.dart
@@ -1,10 +1,42 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-class TradePage extends StatelessWidget {
-  const TradePage({Key key}) : super(key: key);
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class TradePage extends StatefulWidget {
+  TradePage({Key key}) : super(key: key);
+
+  @override
+  _TradePageState createState() => _TradePageState();
+}
+
+class _TradePageState extends State<TradePage>
+    with AutomaticKeepAliveClientMixin {
+  Completer<WebViewController> _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = Completer<WebViewController>();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('市集'),
+      ),
+      body: WebView(
+        initialUrl: 'https://poe.game.qq.com/trade',
+        javascriptMode: JavascriptMode.unrestricted,
+        // onWebViewCreated: (WebViewController webViewController) {
+        //   _controller.complete(webViewController);
+        //   setState(() {});
+        // },
+      ),
+    );
   }
+
+  @override
+  bool get wantKeepAlive => true;
 }
